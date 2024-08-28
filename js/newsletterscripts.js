b122f0082bdfcfbe348b1b9f0805879b9c2dace3
Jannick-Andersen65 <jannickandersen485@gmail.com>
2024-08-28 13:01:37 +0200

    tilføjede newsletter

---
+++ js/newsletterscripts.js
@@ -0,0 +1,57 @@
+// function til åbning af knap
+
+function newsletterBtn() {
+    let show = document.getElementById("newsletter-container");
+    if (show.style.display === "none") {
+      show.style.display = "flex";
+    } else {
+      show.style.display = "none";
+    }
+  }
+
+  // input fleter, med console log
+
+  function opretBtn() {
+    let newsletterName = document.getElementById("input-name").value;
+    let newsletterEmail = document.getElementById("input-email").value;
+    if(newsletterName == "") {
+      alert("Empty Name");
+    }
+
+    else if(newsletterEmail == "")
+      alert("Empty Email")
+
+    else if(newsletterName)
+    console.log("Name: " + newsletterName);
+
+    else(newsletterEmail)
+    console.log("Email: " + newsletterEmail);
+  }
+
+// textindhold to højrel indputfelt
+
+let checklist = ["<p>Nyheder</p>", "<hr>", "<p>Kuponer</p>", "<hr>", "<p>Weekends tilbud</p>"];
+for (let inputPosition = 0; inputPosition < checklist.length; inputPosition++) {
+  document.getElementById("checklist-container").innerHTML += checklist[inputPosition];
+}
+
+// function til at lukke newletter.
+
+function openNewsletter() {
+  console.log("Åbner modalboksen");
+  document.getElementById("newsletter-container").style.display = "flex";
+}
+
+// Lukker modalboksen
+function closeNewsletter() {
+  console.log("Lukker modalboksen");
+  document.getElementById("newsletter-container").style.display = "none";
+}
+
+// Lukker modalboksen, hvis brugeren klikker udenfor
+window.onclick = function (event) {
+  console.log("Klik på vinduet:", event.target);
+  if (event.target == document.getElementById("newsletter-container")) {
+      closeNewsletter();
+  }
+}
